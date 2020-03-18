3b150a454c8afae135d12ed0ad949d2064be5a18
Pedro Varela <pedrovarela86@gmail.com>
2020-03-18 09:35:49 -0400

    Update TaskDetailFragmentTest.kt

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentTest.kt
@@ -28,22 +28,25 @@ class TaskDetailFragmentTest {
 
     private lateinit var repository: TasksRepository
 
+    private lateinit var activeTask: Task
+
+    private lateinit var completedTask: Task
+
     @Before
     fun createRepository() {
         repository = FakeAndroidTestRepository()
         ServiceLocator.tasksRepository = repository
     }
 
-    @After
-    fun cleanupDb() = runBlockingTest {
-        ServiceLocator.resetRepository()
+    @Before
+    fun createTasks() {
+        activeTask = Task("Title", "Description", false)
+        completedTask = Task("Completed task", "Description", true)
     }
 
     @Test
     fun activateTaskDetails_DisplayedInUI() = runBlockingTest {
-
         // Given - Add active (incomplete) task to the DB
-        val activeTask = Task("Title", "Description", false)
         repository.saveTask(activeTask)
 
         // When - Details fragment launched to display the task
@@ -58,4 +61,28 @@ class TaskDetailFragmentTest {
         onView(withId(R.id.task_detail_complete_checkbox)).check(matches(not(isChecked())))
 
     }
+
+    @Test
+    fun completedTaskDetails_DisplayedInUi() = runBlockingTest {
+
+        // Give - Add task to database
+        repository.saveTask(activeTask)
+
+        // When details fragment launched to display task
+        val bundle = TaskDetailFragmentArgs(activeTask.id).toBundle()
+        launchFragmentInContainer<TaskDetailFragment>(bundle, R.style.AppTheme)
+
+        // Then
+        onView(withId(R.id.task_detail_title_text)).check(matches(isDisplayed()))
+        onView(withId(R.id.task_detail_description_text)).check(matches(isDisplayed()))
+        onView(withId(R.id.task_detail_complete_checkbox)).check(matches(isDisplayed()))
+        onView(withId(R.id.task_detail_complete_checkbox)).check(matches(isChecked()))
+    }
+
+
+    @After
+    fun cleanupDb() = runBlockingTest {
+        ServiceLocator.resetRepository()
+    }
+
 }
